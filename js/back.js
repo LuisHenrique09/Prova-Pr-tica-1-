0ce664720ea964a3ffebcc4904af78d7c67cbf6a
Luis <luishenriquecorporativo@gmail.com>
2023-04-11 16:18:06 -0300

    Setimo commit

--- js/back.js
+++ js/back.js
@@ -1,16 +1,32 @@
-var dolar = document.getElementById('dolar')
-var euro = document.getElementById('euro')
-var libra = document.getElementById('libra')
-var inputNum = document.getElementById('inputNum')
-var resultdolar = document.getElementById('resultdolar')
-var resulteuro = document.getElementById('resulteuro')
-var resultlibra = document.getElementById('resultlibra')
+var input = document.getElementById('input')
+var result = document.getElementById('result')
+var inputType = document.getElementById('inputType')
+var resultType = document.getElementById('resultType')
+var inputTypeValue,resultTypeValue;
+
+
+input.addEventListener("keyup",myResult);
+inputType.addEventListener("change",myResult);
+resultType.addEventListener("change",myResult);
+
+
+inputTypeValue = inputType.value;
+resultTypeValue = resultType.value;
+
 
 function myResult(){
 
-    if(){
+inputTypeValue = inputType.value;
+resultTypeValue = resultType.value;
 
-    }
+if (inputTypeValue === "real" && resultTypeValue==="dolar"){
+        result.value = Number(input.value) * 5;
 
+}else if(inputTypeValue ==="real" && resultTypeValue==="euro"){
+    result.value = Number(input.value) * 5,45;
+
+}else if(inputTypeValue ==="real" && resultTypeValue==="libra"){
+    result.value = input.value * 6,45;
+}
 
 }
