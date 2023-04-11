f0aa2478c5e731dbbf51b8cf6319a19d35898a7c
Luis <luishenriquecorporativo@gmail.com>
2023-04-11 15:37:27 -0300

    Quinto Commit

--- js/back.js
+++ js/back.js
@@ -1,8 +1,16 @@
-var input = document.getElementById('input')
+var dolar = document.getElementById('dolar')
+var euro = document.getElementById('euro')
+var libra = document.getElementById('libra')
 var inputNum = document.getElementById('inputNum')
-var result = document.getElementById('result')
+var resultdolar = document.getElementById('resultdolar')
+var resulteuro = document.getElementById('resulteuro')
+var resultlibra = document.getElementById('resultlibra')
 
 function myResult(){
+
+    if(){
+
+    }
 
 
 }
